6bc387da30f6e69de9b5e0f5bb7e3b3a1f1c2e6e
MohanRaj <mraj86698@gmail.com>
2022-07-12 12:37:25 +0530

    Added Find Maximum Problems using Generics

---
+++ src/Generics/GenericsMaximum.java
@@ -0,0 +1,11 @@
+package Generics;
+
+public class GenericsMaximum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Find Maximum Problems using Generics");
+
+	}
+
+}
